40c2946ee5b935bf610d3cdf21308cdc1cffcff5
Robert Corey <rcorey@ziji-media.com>
2015-03-31 11:36:12 -0400

    attempt to handle war, non working

--- main.js
+++ main.js
@@ -1,4 +1,48 @@
 (function() {
+
+  function compareCards (cardA, cardB) {
+    if (cardA.getInt() > cardB.getInt()) {
+      return 1;
+    } else if (cardA.getInt() < cardB.getInt()) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  function handleWar (handA, handB, bountyCards) {
+    //create pot of cards to be awarded;
+    if (!bountyCards) {
+      bountyCards = [];
+    }
+    for (var i = 0; i < 6; i++) {
+      if (i % 2 === 0) {
+        bountyCards.push(handA.shift());
+      } else {
+        bountyCards.push(handB.shift());
+      }
+    }
+    //
+    var cardA = handA.shift();
+    var cardB = handB.shift();
+    switch(compareCards(cardA, cardB)) {
+      case 1:
+        handA.push(cardA);
+        handA.push(cardB);
+        handA.concat(bountyCards);
+        break;
+      case -1:
+        handB.push(cardA);
+        handB.push(cardB);
+        handB.concat(bountyCards); 
+        break;
+      case 0:
+        bountyCards.push(cardA);
+        bountyCards.push(cardB);
+        handleWar(handA, handB, bountyCards);
+    }
+  }
+
   //create deck
   var RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'],
   SUITS = ['s', 'c', 'd', 'h'],
@@ -43,20 +87,22 @@
     }
 
   }
-  console.log(handA);
-  while (handA.length !== 0 || handA.length !== 52) {
+
+  //start game
+  var j = 0;
+  while (j < 100) {
     var cardA = handA.shift();
     var cardB = handB.shift();
     if (cardA.getInt() > cardB.getInt()) {
       handA.push(cardA);
       handA.push(cardB);
-    } else if (cardA.getInt() > cardB.getInt()) {
+    } else if (cardA.getInt() < cardB.getInt()) {
       handB.push(cardA);
       handB.push(cardB);
     } else {
-      //just let handA win for now
-      handA.push(cardA);
-      handA.push(cardA);
+      handleWar(handA, handB);
     }
+    console.log(handA.length);
+    j += 1;
   }
 })();
